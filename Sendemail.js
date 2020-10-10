29a6cd0da0f3a5676ed6f55ef018d4d52ce67dc1
ibrahimAlises <67868688+ibrahimAlises@users.noreply.github.com>
2020-10-10 18:01:50 +0300

    Create Sendemail.js

---
+++ Sendemail.js
@@ -0,0 +1,38 @@
+import smtplib, ssl
+from email.mime.text import MIMEText 
+from email.mime.multipart import MIMEultipart
+from email.mime.base import MIMEBase
+from email import encoders 
+
+sender_email = "(CHANGENAME)"
+receiver_email = "(CHANGENAME)"
+
+message = MIMEultipart("")
+messgae["subject"] = "[CHANGENAME]"
+message["From"] = sender_email
+message ["To"] = receiver_email 
+
+text = """\
+Mesaj
+"""
+
+part1 = MIMEText(text, "plain")
+message.attach(part1)
+
+filepath = "[CHANGENAME]"
+part2 = MIMEBase('application', "octet-stream")
+part2.set_payload(open(filepath, "rb").read()
+encoders.encode_base64(part2)
+              
+part2.add_header('content-Disposition', 'attachment; filename"[CHANGENEME]"')              
+
+message.attach(part2)
+
+# create secure connection with server and send email
+context = ssl.create_default_context()
+server = smtplib.SMTP("mail.securelawfirm.com",587)
+server.starttls()
+server.ehlo_or_helo_if_needed()
+server.sendemail(
+        sender_email, receiver_email, message.as_string()
+  )
